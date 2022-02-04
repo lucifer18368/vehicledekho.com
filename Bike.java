d4fa342e6ad7ab7cb8186d40b630ed57f47df5a4
parth-sevenx <98866957+parth-sevenx@users.noreply.github.com>
2022-02-04 09:15:54 +0530

    Add files via upload

---
+++ Bike.java
@@ -0,0 +1,20 @@
+package com.vehicledekho;
+
+public class Bike extends Vehicle {
+	int power;
+	String noofGears;
+	public int getpower() {
+		return power;
+	}
+	public void setpower(int power) {
+		this.power = power;
+	}
+	public String getnoofGears() {
+		return noofGears;
+	}
+	public void setnoofGears(String noofGears) {
+		this.noofGears = noofGears;
+	}
+	
+	}
+
---
+++ Main.java
@@ -0,0 +1,32 @@
+package com.vehicledekho;
+import java.util.ArrayList;
+import com.vehicledekho.ui.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Bike bike = new Bike();
+        bike.setmodel("hayabusa");
+        bike.setmileage(55);
+        bike.setprice(120000);
+        bike.settype("sports");
+        bike.setpower(200);
+        bike.setnoofGears("5");
+       
+
+        Car car = new Car();
+        car.setbrand("Ducati");
+        car.setmodel("Ver");
+        car.setmileage(12);
+        car.setprice(1000000);
+        car.setcategory("Offroad");
+
+
+        ArrayList <Vehicle> vehicles = new ArrayList<>();
+        vehicles.add(bike);
+        vehicles.add(car);
+
+        VehicleDisplay.displayVehicle(vehicles);
+	}
+
+}
