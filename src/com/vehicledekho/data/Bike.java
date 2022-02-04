62681579b33b98d26a188fed937633be99658e5e
parth-sevenx <98866957+parth-sevenx@users.noreply.github.com>
2022-02-04 11:18:11 +0530

    Add files via upload

---
+++ src/com/vehicledekho/data/Bike.java
@@ -0,0 +1,20 @@
+package com.vehicledekho.data;
+
+public class Bike extends Vehicle {
+	int power;
+	String numberofGears;
+	public int getpower() {
+		return power;
+	}
+	public void setpower(int power) {
+		this.power = power;
+	}
+	public String getnumberOfGears() {
+		return numberofGears;
+	}
+	public void setnumberOfGears(String numberofgears) {
+		this.numberofGears = numberofgears;
+	}
+	
+	}
+
---
+++ src/com/vehicledekho/data/Car.java
@@ -0,0 +1,21 @@
+package com.vehicledekho.data;
+
+public class Car extends Vehicle {
+	String brand;
+	String category;
+	
+	public String getbrand() {
+		return brand;
+	}
+	public void setbrand(String brand) {
+		this.brand = brand;
+	}
+	public String getcategory() {
+		return category;
+	}
+	public void setcategory(String category) {
+		this.category = category;
+	}
+}
+		
+	
